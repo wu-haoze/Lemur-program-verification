5b4d471f3f9dd1a5f0b78f3a666545e806348f7f
anwu1219 <haozewu@stanford.edu>
2023-07-24 09:34:54 -0700

    update gitignore

--- build.sh
+++ build.sh
@@ -1,18 +1,23 @@
 #!/usr/bin/env bash
 
-
+if [ ! -f sv-benchmarks-main.zip ]
+then
+    wget https://gitlab.com/sosy-lab/benchmarking/sv-benchmarks/-/archive/main/sv-benchmarks-main.zip
+    rm -rf sv-benchmarks-main
+    unzip sv-benchmarks-main.zip
+fi
 
 if [ ! -f cbmc.zip ]
 then
     wget https://gitlab.com/sosy-lab/sv-comp/archives-2023/raw/svcomp23/2023/cbmc.zip
+    rm -rf cbmc
+    unzip cbmc.zip
 fi
 if [ ! -f uautomizer.zip ]
 then
     wget https://gitlab.com/sosy-lab/sv-comp/archives-2023/raw/svcomp23/2023/uautomizer.zip
+    rm -rf UAutomizer-linux uautomizer
+    unzip uautomizer.zip
+    mv UAutomizer-linux uautomizer
 fi
 
-rm -rf cbmc UAutomizer-linux uautomizer
-
-unzip cbmc.zip
-unzip uautomizer.zip
-mv UAutomizer-linux uautomizer
